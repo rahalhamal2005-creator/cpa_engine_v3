bd9d5551904fb98cc0c5efeaed67837bbdcbc216
Rahal Hamal <rahalhamal2005@gmail.com>
2026-04-09 01:32:42 +0100

    Fix database connection for Neon PostgreSQL

--- backend/core/database.py
+++ backend/core/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import settings
 
-engine = create_engine(settings.db_url, connect_args={"check_same_thread": False})
+connect_args = {"check_same_thread": False} if "sqlite" in settings.db_url else {}
+
+engine = create_engine(settings.db_url, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
